68fdc0b3f0430f31ecb5f24a332f32f8844f0a2a
Pauline <gokowanjiru@gmail.com>
2023-02-02 08:06:46 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var full_name = "Pauline Wanjiru"
+    var age = 24
+    var phone_number = +254790733892
+    var weightInKgs = 58
+    var isKenyan = "true"
+    var isNotKenyan = "false"
+    
+}
